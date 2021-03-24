9b90ddb747467ebbfcdf23fa496917f71b6d8305
liusijia <771259640@qq.com>
2021-03-24 15:57:19 +0800

    add github actions

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: '学习小册',
   mode: 'doc',
+  base: '/note',
+  publicPath: '/note/',
+  exportStatic: {},
   // more config: https://d.umijs.org/config
 });
